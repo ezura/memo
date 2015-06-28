19bcf76b1e385d34e3000d92d117e8fd6ada3557
Yuka Ezura <yuka.ezura@gmail.com>
2015-06-28 13:04:11 +0900

    【画像解析】動作確認用の画像とレイヤを追加

---
+++ memo/AddInformation/AddInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddInformationViewController.h
+//  memo
+//
+//  Created by ezura on 6/27/15.
+//  Copyright (c) 2015 ezura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddInformationViewController : UIViewController
+
+@end
---
+++ memo/AddInformation/DrawLayerView.m
@@ -0,0 +1,19 @@
+//
+//  DrawLayerView.m
+//  memo
+//
+//  Created by ezura on 6/27/15.
+//  Copyright (c) 2015 ezura. All rights reserved.
+//
+
+#import "DrawLayerView.h"
+
+@implementation DrawLayerView
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    
+}
+
+@end
