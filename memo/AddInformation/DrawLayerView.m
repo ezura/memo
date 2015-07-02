eff8dc0a99d55192625c0a072e479d294854ac85
Yuka Ezura <yuka.ezura@gmail.com>
2015-07-02 19:31:44 +0900

    【環境構築】不要なファイル削除

--- memo/AddInformation/DrawLayerView.m
+++ memo/AddInformation/DrawLayerView.m
@@ -13,7 +13,6 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    
 }
 
 @end
