e5ba2b70c0f85bfd6d49c673fff81b90b5ef1c39
Yuka Ezura <yuka.ezura@gmail.com>
2015-07-05 11:53:37 +0900

    【おかず編集】おかずを表すクラスを追加

---
+++ memo/Analyzer/ColorAnalyzer.h
@@ -0,0 +1,13 @@
+//
+//  ColorAnalyzer.h
+//  memo
+//
+//  Created by ezura on 7/2/15.
+//  Copyright (c) 2015 ezura. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ColorAnalyzer : NSObject
+
+@end
